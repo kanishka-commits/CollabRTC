86ba1bea757cbe627d8a7db0b8d5bea12d7f5275
kanishka-commits <kanishka03p@gmail.com>
2025-10-18 16:12:48 +0530

    Update controllers

--- Server/src/controllers/user.controller.js
+++ Server/src/controllers/user.controller.js
@@ -2,6 +2,8 @@ import httpStatus from "http-status";
 import {User} from "../models/user.model.js"
 import bcrypt,{hash} from "bcrypt"
 import crypto from "crypto"
+import { Meeting } from "../models/meeting.model.js";
+
 const login=async(req,res)=>{
     const {username, password}=req.body;
     if(!username || !password) {
@@ -12,7 +14,6 @@ const login=async(req,res)=>{
         if(!user){
             return res.send(httpStatus.NOT_FOUND).json({message:"User Not Found!"});
         }
-
         if(bcrypt.compare(password,user.password)){
             let token=crypto.randomBytes(20).toString("hex");
             console.log("user is",user[0]);
@@ -49,4 +50,35 @@ const register = async(req,res)=>{
     }
 }
 
-export {login, register};
+const getUserHistory = async (req, res) => {
+    const { token } = req.query;
+
+    try {
+        const user = await User.findOne({ token: token });
+        const meetings = await Meeting.find({ user_id: user.username })
+        res.json(meetings)
+    } catch (e) {
+        res.json({ message: `Something went wrong ${e}` })
+    }
+}
+
+const addToHistory = async (req, res) => {
+    const { token, meeting_code } = req.body;
+
+    try {
+        const user = await User.findOne({ token: token });
+
+        const newMeeting = new Meeting({
+            user_id: user.username,
+            meetingCode: meeting_code
+        })
+
+        await newMeeting.save();
+
+        res.status(httpStatus.CREATED).json({ message: "Added code to history" })
+    } catch (e) {
+        res.json({ message: `Something went wrong ${e}` })
+    }
+}
+
+export {login, register, getUserHistory, addToHistory};
